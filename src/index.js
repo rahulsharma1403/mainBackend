9efc599605fa063a94c18d23172569ba3f09902a
Rohit <rohitkchaudhary1718@gmail.com>
2026-03-13 16:57:11 +0530

    DB connection approach 1 in index.js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { DB_NAME } from "./constants.js";
+
+dotenv.config();
+
+/*
+
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERRR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+})();
+
+*/
